39cad9275f8601a12584459203518b8666b8be6c
MadanKumar <madan739@gmail.com>
2020-08-05 10:03:14 +0530

    Added Logger

--- spring-security-mvc/src/main/java/mk/springdemo/controller/DemoController.java
+++ spring-security-mvc/src/main/java/mk/springdemo/controller/DemoController.java
@@ -1,11 +1,15 @@
 package mk.springdemo.controller;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
 
+	Logger logger = Logger.getLogger(getClass().getName());
+	
 	@GetMapping("/")
 	public String showHome() {
 		return "home";
@@ -13,11 +17,13 @@ public class DemoController {
 	
 	@GetMapping("/leaders")
 	public String showLeaders() {
+		logger.info("In leaders");
 		return "leaders";
 	}
 	
 	@GetMapping("/systems")
 	public String showSystems() {
+		logger.info("In systems");
 		return "systems";
 	}
 }
